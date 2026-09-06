9c7abaccf6fa55ce77f12a63b0e80a1375956b5e
kgdee <kgdee7603@gmail.com>
2026-09-07 02:33:48 +0800

    commit

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,76 @@
 const PROJECT_NAME = "html-starter"
+
+const ARTWORKS_URL = "assets/images/artworks"
+
+const CARDS_DATA = [
+  {
+    name: "Archangel Warrior",
+    artwork: `${ARTWORKS_URL}/archangel-warrior.jpg`,
+    cost: 7,
+    type: "Creature",
+    rarity: "Epic"
+  },
+  {
+    name: "Elven Archer",
+    artwork: `${ARTWORKS_URL}/archer.jpg`,
+    cost: 2,
+    type: "Creature",
+    rarity: "Common"
+  },
+  {
+    name: "Armored Knight",
+    artwork: `${ARTWORKS_URL}/armored-knight.jpg`,
+    cost: 4,
+    type: "Creature",
+    rarity: "Uncommon"
+  },
+  {
+    name: "Elven Princess",
+    artwork: `${ARTWORKS_URL}/blonde-elf-princess.jpg`,
+    cost: 3,
+    type: "Creature",
+    rarity: "Rare"
+  },
+  {
+    name: "Frost Dragon",
+    artwork: `${ARTWORKS_URL}/blue-ice-dragon.jpg`,
+    cost: 8,
+    type: "Dragon",
+    rarity: "Legendary"
+  },
+  {
+    name: "High Cleric",
+    artwork: `${ARTWORKS_URL}/cleric.jpg`,
+    cost: 5,
+    type: "Creature",
+    rarity: "Rare"
+  },
+  {
+    name: "Dread Knight",
+    artwork: `${ARTWORKS_URL}/dark-armored-knight.jpg`,
+    cost: 6,
+    type: "Creature",
+    rarity: "Epic"
+  },
+  {
+    name: "Empress of Shadows",
+    artwork: `${ARTWORKS_URL}/dark-empress.jpg`,
+    cost: 8,
+    type: "Creature",
+    rarity: "Legendary"
+  },
+  {
+    name: "Dark Lord",
+    artwork: `${ARTWORKS_URL}/dark-lord.jpg`,
+    cost: 9,
+    type: "Creature",
+    rarity: "Legendary"
+  },
+  {
+    name: "Shadow Sorceress",
+    artwork: `${ARTWORKS_URL}/dark-sorceress.jpg`,
+    cost: 5,
+    type: "Spellcaster",
+    rarity: "Epic"
+  }
+];
